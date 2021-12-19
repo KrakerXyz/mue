bd5cf12de56f1a3d308272ab72000214c635b85a
Josh Kraker <joshkrak@gmail.com>
2021-12-19 00:22:38 -0500

    Fix expand all

--- web/src/components/global/VirtualList.vue
+++ web/src/components/global/VirtualList.vue
@@ -1,6 +1,6 @@
 <template>
    <div ref="parent">
-      <div v-for="item of partial" :key="item.id">
+      <div v-for="item of displayItems" :key="item.index" :data-index="item.index">
          <slot :item="item.item" :index="item.index"></slot>
       </div>
       <!-- min-height is intentional here. I tried with height but it doesn't haven an effect on the scroll -->
@@ -9,8 +9,7 @@
 </template>
 
 <script lang="ts">
-   import { v4 } from 'uuid';
-   import { computed, defineComponent, nextTick, reactive, ref, watch } from 'vue';
+   import { computed, defineComponent, nextTick, onUnmounted, reactive, ref, watch } from 'vue';
 
    export default defineComponent({
       props: {
@@ -20,59 +19,78 @@
          const vms = computed(() => {
             return props.items.map<ItemVm>((i, index) => {
                return {
-                  id: v4(),
                   index,
                   item: i,
-                  height: null,
                };
             });
          });
 
          const parent = ref<HTMLDivElement | undefined>();
-         const partial = ref([] as any[]);
+         const displayItems = ref([] as any[]);
 
-         const overflow = reactive({
+         const overflow = reactive<Overflow>({
             top: 0,
             bottom: 0,
          });
 
-         watch([vms, parent], async (x) => {
-            const [vms, div] = x;
+         let observer: ResizeObserver | null = null;
 
+         const reset = async () => {
+            observer?.disconnect();
+            displayItems.value = reactive([]);
             overflow.bottom = 0;
             overflow.top = 0;
+            await nextTick();
+         };
+
+         watch([vms, parent], async (x) => {
+            const [vms, div] = x;
 
             if (!div) {
                return;
             }
 
-            await nextTick();
-
-            partial.value = reactive([]);
             console.debug(`Vms updated ${vms.length}`), { immediate: true };
+            await reset();
+            await fit(vms, displayItems.value, div, overflow);
 
-            while (div.clientHeight >= div.scrollHeight && partial.value.length < vms.length) {
-               partial.value.push(vms[partial.value.length]);
-               await nextTick();
-            }
-
-            if (partial.value.length) {
-               console.debug(`Loaded ${partial.value.length} records into list`);
-            }
-
-            const avgHeight = div.clientHeight / partial.value.length;
-            const remainingRecords = vms.length - partial.value.length;
-            overflow.bottom = remainingRecords * avgHeight;
+            observer = new ResizeObserver(async () => {
+               console.debug('Resize triggered');
+               await fit(vms, displayItems.value, div, overflow);
+            });
+            observer.observe(div);
          });
 
-         return { partial, parent, overflow };
+         onUnmounted(() => observer?.disconnect());
+
+         return { displayItems, parent, overflow };
       },
    });
 
+   async function fit(srcItems: ItemVm[], displayItems: ItemVm[], containerDiv: HTMLDivElement, overflow: Overflow): Promise<void> {
+      const origDisplayLen = displayItems.length;
+      while (containerDiv.clientHeight >= containerDiv.scrollHeight - (overflow.bottom + overflow.top) && displayItems.length < srcItems.length) {
+         displayItems.push(srcItems[displayItems.length]);
+         await nextTick();
+      }
+
+      const newItemsLen = displayItems.length - origDisplayLen;
+      if (newItemsLen) {
+         console.log(`${newItemsLen > 0 ? 'Added' : 'Removed'} ${Math.abs(newItemsLen)} items from display list`);
+      }
+
+      const avgHeight = containerDiv.clientHeight / displayItems.length;
+      const remainingRecords = srcItems.length - displayItems.length;
+      overflow.bottom = remainingRecords * avgHeight;
+   }
+
+   interface Overflow {
+      top: number;
+      bottom: number;
+   }
+
    interface ItemVm {
-      id: string;
       index: number;
       item: any;
-      height: number | null;
    }
 </script>
